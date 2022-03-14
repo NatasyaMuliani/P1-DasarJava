6e84bbc31dd8f7afcb25f26bdc813289a1f6c9a7
PC 06 <natasyamuliani1@gmail.com>
2022-03-14 03:21:04 -0700

    percobaan mencetak literal string

---
+++ src/M_MencetakLiteralString.java
@@ -0,0 +1,6 @@
+public class M_MencetakLiteralString {
+    public static void main(String[] args) {
+        String welcome="pemrograman berorientasi objek dengan java";
+        System.out.println(welcome);
+    }
+}
