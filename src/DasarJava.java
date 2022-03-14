d092c3a9fa8fae4cbb442fc19f3c57b22c88a94b
PC 06 <natasyamuliani1@gmail.com>
2022-03-14 01:11:47 -0700

    initial project pertemuan 1 dasar java

---
+++ src/DasarJava.java
@@ -0,0 +1,14 @@
+public class DasarJava {
+    public static void main (String []args){
+        System.out.println("dasar pemograman java");
+        // contoh komentar satu baris
+
+        /*
+        contoh komentar lebih satu baris
+         */
+
+        /**
+         * java Doc --> baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
